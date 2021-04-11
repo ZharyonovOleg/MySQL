a94180ee2d85f9584e66325cba3e8e574d03ccfb
Oleg <zharyonovoleg@gmail.com>
2021-04-11 22:37:34 +0500

    upd5 hw4

--- HW04.sql
+++ HW04.sql
@@ -3,10 +3,10 @@ delete from messages
 where created_at > NOW()
 ;
 
-alter table profiles add column is_active BOOL default true;
-
 
 # 3
+alter table profiles add column is_active BOOL default true;
+
 update profiles 
 set is_active = false
 where birthday > '2003-04-11'
